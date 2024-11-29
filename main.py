46a49a3da81803cb365aeb4659ddf57db8e0c2a3
ddeeniska <denis722010@gmail.com>
2024-11-29 17:30:20 +0500

    first commit

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import sys
 from random import randint
-from PyQt6 import uic
+
 from PyQt6.QtCore import QPoint
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QApplication, QMainWindow
-class MyWidget(QMainWindow):
+from ui import Ui_MainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.circles = []
-        self.PushButton.clicked.connect(self.add)
+        self.pushButton.clicked.connect(self.add)
+
     def add(self):
         r = randint(10, 50)
         x = randint(50, 550)
         y = randint(50, 550)
-        self.circles.append((x, y, r))
+        rr = randint(0, 255)
+        gg = randint(0, 255)
+        bb = randint(0, 255)
+        self.circles.append((x, y, r, rr, gg, bb))
         self.update()
+
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         self.draw_circles(qp)
         qp.end()
+
     def draw_circles(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        for x, y, r in self.circles:
+        for x, y, r, rr, gg, bb in self.circles:
+            qp.setBrush(QColor(rr, gg, bb))
             qp.drawEllipse(QPoint(x, y), r, r)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
